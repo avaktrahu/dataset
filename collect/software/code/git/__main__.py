6e24bc754ded5bb6958f7a1ed0a0b9d9f8cf491d
Avaktrahu Shashthishalaka <avaktrahu@gmail.com>
2025-08-23 14:07:08 +0000

    [collect] Updating git repositories

--- collect/software/code/git/__main__.py
+++ collect/software/code/git/__main__.py
@@ -1,4 +1,5 @@
 from .main import sync
 
 sync('.')
-sync('https://github.com/avaktrahu/jailbreak-nsfw-filters.git')
+sync('https://github.com/avaktrahu/nsfw-prompts.git')
+sync('https://github.com/avaktrahu/avaktrahu.hashnode.dev.git')
